eff3c27ae9e5f5b24b4e1a7c39f99d7b961212e5
iammahesh09 <iammahesh.nlr@gmail.com>
2018-08-30 17:41:50 +0530

    Split Two way Binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  title: any = "MovieDump";
+
+  username: string = "Mahesh";
+
+  username2: string = "Chejarla";
+
+  username3: string = "Reddy";
+
+  userName(name) {
+    this.username3 = name
+  }
+
   constructor() { }
 
   ngOnInit() {
